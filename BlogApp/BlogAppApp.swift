6ba8347f7b0c00e6cace16c6985c1cac939d9b4a
roman-luzgin <luzginn@gmail.com>
2021-09-11 14:56:54 +0200

    Initial Commit

---
+++ BlogApp/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  BlogApp
+//
+//  Created by Roman Luzgin on 11.09.21.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlogApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlogApp
+//
+//  Created by Roman Luzgin on 11.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
